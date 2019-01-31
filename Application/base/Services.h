b91f4e9a7d2a666fe776785a477497e508e6861f
Johnny Martin <970803555@qq.com>
2019-02-01 01:56:30 +0800

    peer_manager_init拆分

--- Application/base/Services.h
+++ Application/base/Services.h
@@ -10,12 +10,36 @@
 #include "bsp_btn_ble.h"
 #include "nrf_ble_qwr.h"
 
+#include "ble.h"
+#include "ble_err.h"
+#include "ble_hci.h"
+#include "ble_srv_common.h"
+#include "ble_advertising.h"
+#include "ble_advdata.h"
+#include "ble_hids.h"
+#include "ble_bas.h"
+#include "ble_dis.h"
+#include "ble_conn_params.h"
+
+#include "peer_manager.h"
+
 #define SCHED_MAX_EVENT_DATA_SIZE           APP_TIMER_SCHED_EVENT_DATA_SIZE            /**< Maximum size of scheduler events. */
 #ifdef SVCALL_AS_NORMAL_FUNCTION
 #define SCHED_QUEUE_SIZE                    20                                         /**< Maximum number of events in the scheduler queue. More is needed in case of Serialization. */
 #else
 #define SCHED_QUEUE_SIZE                    10                                         /**< Maximum number of events in the scheduler queue. */
 #endif
+
+
+#define SEC_PARAM_BOND                      1                                          /**< Perform bonding. */
+#define SEC_PARAM_MITM                      0                                          /**< Man In The Middle protection not required. */
+#define SEC_PARAM_LESC                      0                                          /**< LE Secure Connections not enabled. */
+#define SEC_PARAM_KEYPRESS                  0                                          /**< Keypress notifications not enabled. */
+#define SEC_PARAM_IO_CAPABILITIES           BLE_GAP_IO_CAPS_NONE                       /**< No I/O capabilities. */
+#define SEC_PARAM_OOB                       0                                          /**< Out Of Band data not available. */
+#define SEC_PARAM_MIN_KEY_SIZE              7                                          /**< Minimum encryption key size. */
+#define SEC_PARAM_MAX_KEY_SIZE              16                                         /**< Maximum encryption key size. */
+
 
 void scheduler_init(void);
 
@@ -28,4 +52,6 @@ void buttons_leds_init(bool * p_erase_bonds, bsp_event_callback_t bsp_event_hand
 void timers_init(void);
 
 void qwr_init(nrf_ble_qwr_t * p_qwr);
+
+void peer_manager_init(pm_evt_handler_t pm_evt_handler);
 #endif
