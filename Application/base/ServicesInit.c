6c6dccd698d58eaf18373892db7138f377b6918c
Johnny Martin <970803555@qq.com>
2019-02-11 18:34:51 +0800

    GAP、GATT => ServicesInit module

--- Application/base/ServicesInit.c
+++ Application/base/ServicesInit.c
@@ -48,3 +48,68 @@ void ppi_init(void)
 {
     APP_ERROR_CHECK(nrf_drv_ppi_init());
 }
+
+/**@brief Function for initializing Device Information Service.
+ */
+void dis_init(void)
+{
+    ret_code_t       err_code;
+    ble_dis_init_t   dis_init_obj;
+    ble_dis_pnp_id_t pnp_id;
+
+    pnp_id.vendor_id_source = PNP_ID_VENDOR_ID_SOURCE;
+    pnp_id.vendor_id        = PNP_ID_VENDOR_ID;
+    pnp_id.product_id       = PNP_ID_PRODUCT_ID;
+    pnp_id.product_version  = PNP_ID_PRODUCT_VERSION;
+
+    memset(&dis_init_obj, 0, sizeof(dis_init_obj));
+
+    ble_srv_ascii_to_utf8(&dis_init_obj.manufact_name_str, MANUFACTURER_NAME);
+    dis_init_obj.p_pnp_id = &pnp_id;
+
+    BLE_GAP_CONN_SEC_MODE_SET_ENC_NO_MITM(&dis_init_obj.dis_attr_md.read_perm);
+    BLE_GAP_CONN_SEC_MODE_SET_NO_ACCESS(&dis_init_obj.dis_attr_md.write_perm);
+
+    err_code = ble_dis_init(&dis_init_obj);
+    APP_ERROR_CHECK(err_code);
+}
+
+/**@brief Function for the GAP initialization.
+ *
+ * @details This function sets up all the necessary GAP (Generic Access Profile) parameters of the
+ *          device including the device name, appearance, and the preferred connection parameters.
+ */
+void gap_params_init(void)
+{
+    ret_code_t              err_code;
+    ble_gap_conn_params_t   gap_conn_params;
+    ble_gap_conn_sec_mode_t sec_mode;
+
+    BLE_GAP_CONN_SEC_MODE_SET_OPEN(&sec_mode);
+
+    err_code = sd_ble_gap_device_name_set(&sec_mode,
+                                          (const uint8_t *)DEVICE_NAME,
+                                          strlen(DEVICE_NAME));
+    APP_ERROR_CHECK(err_code);
+
+    err_code = sd_ble_gap_appearance_set(BLE_APPEARANCE_HID_KEYBOARD);
+    APP_ERROR_CHECK(err_code);
+
+    memset(&gap_conn_params, 0, sizeof(gap_conn_params));
+
+    gap_conn_params.min_conn_interval = MIN_CONN_INTERVAL;
+    gap_conn_params.max_conn_interval = MAX_CONN_INTERVAL;
+    gap_conn_params.slave_latency     = SLAVE_LATENCY;
+    gap_conn_params.conn_sup_timeout  = CONN_SUP_TIMEOUT;
+
+    err_code = sd_ble_gap_ppcp_set(&gap_conn_params);
+    APP_ERROR_CHECK(err_code);
+}
+
+/**@brief Function for initializing the GATT(Generic Attribute Profile) module.
+ */
+void gatt_init(void)
+{
+    ret_code_t err_code = nrf_ble_gatt_init(&m_gatt, NULL);
+    APP_ERROR_CHECK(err_code);
+}
--- Application/base/ServicesInit.h
+++ Application/base/ServicesInit.h
@@ -26,6 +26,8 @@
 #include "nrf_drv_ppi.h"
 #include "nrfx_ppi.h"
 
+#include "nrf_ble_gatt.h"
+
 #define SCHED_MAX_EVENT_DATA_SIZE           APP_TIMER_SCHED_EVENT_DATA_SIZE            /**< Maximum size of scheduler events. */
 #ifdef SVCALL_AS_NORMAL_FUNCTION
 #define SCHED_QUEUE_SIZE                    20                                         /**< Maximum number of events in the scheduler queue. More is needed in case of Serialization. */
@@ -34,10 +36,24 @@
 #endif
 
 
+#define PNP_ID_VENDOR_ID_SOURCE             0x02                                       /**< Vendor ID Source. */
+#define PNP_ID_VENDOR_ID                    0x1915                                     /**< Vendor ID. */
+#define PNP_ID_PRODUCT_ID                   0xEEEE                                     /**< Product ID. */
+#define PNP_ID_PRODUCT_VERSION              0x0001                                     /**< Product Version. */
+			
+#define MANUFACTURER_NAME                   "JM-Build"                      		   /**< Manufacturer. Will be passed to Device Information Service. */
+#define DEVICE_NAME                         "CozyType_Keyboard"                        /**< Name of device. Will be included in the advertising data. */
+
+#define MIN_CONN_INTERVAL                   MSEC_TO_UNITS(7.5, UNIT_1_25_MS)           /**< Minimum connection interval (7.5 ms) */
+#define MAX_CONN_INTERVAL                   MSEC_TO_UNITS(30, UNIT_1_25_MS)            /**< Maximum connection interval (30 ms). */
+#define SLAVE_LATENCY                       6                                          /**< Slave latency. */
+#define CONN_SUP_TIMEOUT                    MSEC_TO_UNITS(430, UNIT_10_MS)             /**< Connection supervisory timeout (430 ms). */
+
+NRF_BLE_GATT_DEF(m_gatt);                                           				   /**< GATT module instance. */
+
+
 void scheduler_init(void);
 
-/**@brief Function for initializing the nrf log module.
- */
 void log_init(void);
 
 void timers_init(void);
@@ -45,4 +61,11 @@ void timers_init(void);
 void gpiote_init(void);
 
 void ppi_init(void);
+
+void dis_init(void);
+
+void gap_params_init(void);
+
+void gatt_init(void);
+
 #endif
