f64ec4e8ad255c1ff2c0732a0026cea2438b2ad2
Johnny Martin <970803555@qq.com>
2019-02-27 21:04:51 +0800

    update: get capslock state error; disconnect cozytype error

--- Application/base/HIDService.c
+++ Application/base/HIDService.c
@@ -2,35 +2,32 @@
 #include "app_error.h"
 #include "nrf_log.h"
 
-#define REPORT_ID_KEYBOARD                  0x01                                       	/**< Position of the modifier byte in the Input Report. */
-#define REPORT_ID_MOUSE                   	0x02                                        /**< Position of the modifier byte in the Input Report. */
+#define BASE_USB_HID_SPEC_VERSION           0x0101                                     	/**< Version number of base USB HID Specification implemented by this application. */
 
+#define INPUT_REPORT_COUNT		    		2
+#define INPUT_REPORT_KBD_INDEX              0                                          	/**< Index of Input Report. */
+#define INPUT_REPORT_MOUSE_INDEX            1                                          	/**< Index of Input Report. */
+#define INPUT_REPORT_KEYBOARD_ID            0x01                                       	/**< Position of the modifier byte in the Input Report. */
+#define INPUT_REPORT_MOUSE_ID              	0x02                                        /**< Position of the modifier byte in the Input Report. */
 #define INPUT_REPORT_KEYS_MAX_LEN           8                                          	/**< Maximum length of the Input Report characteristic. */
-#define SCAN_CODE_POS_BOOT_MODE             2                                          	/**< The start position of the key scan code in a HID Report. */
-#define SCAN_CODE_POS_REPORT_MODE           3                                          	/**< The start position of the key scan code in a HID Report. */
-#define KEYS_SIZE_REPORT_MODE	            5                                          	/**< The start position of the key scan code in a HID Report. */
-#define KEYS_SIZE_BOOT_MODE	 	            6                                          	/**< The start position of the key scan code in a HID Report. */
-
-
+#define INPUT_REPORT_MOUSE_MAX_LEN          4                                          	/**< Maximum length of the Input Report characteristic. */
 
 #define MODIFIER_KEY_POS                    0                                          	/**< Position of the modifier byte in the Input Report. */
-#define BASE_USB_HID_SPEC_VERSION           0x0101                                     	/**< Version number of base USB HID Specification implemented by this application. */
-#define OUTPUT_REPORT_INDEX                 0                                          	/**< Index of Output Report. */
-#define OUTPUT_REPORT_MAX_LEN               1                                          	/**< Maximum length of Output Report. */
-#define INPUT_REPORT_KEYS_INDEX             0                                          	/**< Index of Input Report. */
-#define INPUT_REPORT_MOUSE_INDEX            1                                          	/**< Index of Input Report. */
-#define INPUT_REPORT_MOUSE             		1                                          	/**< Index of Input Report. */
-#define OUTPUT_REPORT_BIT_MASK_CAPS_LOCK    0x02                                       	/**< CAPS LOCK bit in Output Report (based on 'LED Page (0x08)' of the Universal Serial Bus HID Usage Tables). */
-#define INPUT_REP_REF_ID                    0                                          	/**< Id of reference to Keyboard Input Report. */
-#define OUTPUT_REP_REF_ID                   0                                          	/**< Id of reference to Keyboard Output Report. */
-#define MAX_BUFFER_ENTRIES                  5                                           /**< Number of elements that can be enqueued */
+#define SCAN_CODE_POS		                2                                          	/**< The start position of the key scan code in a HID Report. */
 
-bool              			m_in_boot_mode = false;  									/**< Current protocol mode. */
-extern uint16_t 			m_conn_handle;												/**< Handle of the current connection. */
+#define OUTPUT_REPORT_COUNT                 1                                          	/**< Maximum length of Output Report. */
+#define OUTPUT_REPORT_MAX_LEN               1                                          	/**< Maximum length of Output Report. */
+#define OUTPUT_REPORT_INDEX                 0                                          	/**< Index of Output Report. */
+#define OUTPUT_REPORT_ID                    0                                          	/**< Id of reference to Keyboard Output Report. */
+
+#define OUTPUT_REPORT_BIT_MASK_CAPS_LOCK    0x02                                       	/**< CAPS LOCK bit in Output Report (based on 'LED Page (0x08)' of the Universal Serial Bus HID Usage Tables). */
+
+bool              							m_in_boot_mode = false;						/**< Current protocol mode. */
+extern uint16_t 							m_conn_handle;								/**< Handle of the current connection. */
 BLE_HIDS_DEF(m_hids,                                                					/**< Structure used to identify the HID service. */
              NRF_SDH_BLE_TOTAL_LINK_COUNT,
              INPUT_REPORT_KEYS_MAX_LEN,
-             OUTPUT_REPORT_MAX_LEN);
+             OUTPUT_REPORT_COUNT);
 
 			 
 //do not know how to send HID mouse report with REPORT_ID 0x02
@@ -42,7 +39,7 @@ static uint8_t keyboard_mouse_report_map_data[] = {
    0x05, 0x01,                    // USAGE_PAGE (Generic Desktop)
    0x09, 0x06,                    // USAGE (Keyboard)
    0xa1, 0x01,                    // COLLECTION (Application)
-   0x85, REPORT_ID_KEYBOARD,      //   REPORT_ID (1)
+   0x85, INPUT_REPORT_KEYBOARD_ID,//   REPORT_ID (1)
    0x05, 0x07,                    //   USAGE_PAGE (Keyboard)
 	
    0x19, 0xe0,                    //   USAGE_MINIMUM (Keyboard LeftControl)
@@ -57,7 +54,7 @@ static uint8_t keyboard_mouse_report_map_data[] = {
    0x75, 0x08,                    //   REPORT_SIZE (8)
    0x81, 0x01,                    //   INPUT (Cnst,Ary,Abs)
 	
-   0x95, 0x05,                    //   REPORT_COUNT (6)
+   0x95, 0x06,                    //   REPORT_COUNT (6)
    0x75, 0x08,                    //   REPORT_SIZE (8)
    0x15, 0x00,                    //   LOGICAL_MINIMUM (0)
    0x25, 0x65,                    //   LOGICAL_MAXIMUM (101)
@@ -72,6 +69,7 @@ static uint8_t keyboard_mouse_report_map_data[] = {
    0x19, 0x01,       			  //   Usage Minimum (1)
    0x29, 0x05,       			  //   Usage Maximum (5)
    0x91, 0x02,       			  //   Output (Data, Variable, Absolute), Led report
+   
    0x95, 0x01,       			  //   Report Count (1)
    0x75, 0x03,       			  //   Report Size (3)
    0x91, 0x01,       			  //   Output (Data, Variable, Absolute), Led report padding
@@ -89,9 +87,10 @@ static uint8_t keyboard_mouse_report_map_data[] = {
    0x05, 0x01,                    // USAGE_PAGE (Generic Desktop)
    0x09, 0x02,                    // USAGE (Mouse)
    0xa1, 0x01,                    // COLLECTION (Application)
-   0x85, REPORT_ID_MOUSE,         //   REPORT_ID (2)
+   0x85, INPUT_REPORT_MOUSE_ID,   //   REPORT_ID (2)
    0x09, 0x01,                    //   USAGE (Pointer)
    0xA1, 0x00,                    //   COLLECTION (Physical)
+   
    0x05, 0x09,                    //     USAGE_PAGE (Button)
    0x19, 0x01,                    //     USAGE_MINIMUM
    0x29, 0x03,                    //     USAGE_MAXIMUM
@@ -100,9 +99,11 @@ static uint8_t keyboard_mouse_report_map_data[] = {
    0x95, 0x03,                    //     REPORT_COUNT (3)
    0x75, 0x01,                    //     REPORT_SIZE (1)
    0x81, 0x02,                    //     INPUT (Data,Var,Abs)
+   
    0x95, 0x01,                    //     REPORT_COUNT (1)
    0x75, 0x05,                    //     REPORT_SIZE (5)
    0x81, 0x03,                    //     INPUT (Const,Var,Abs)
+   
    0x05, 0x01,                    //     USAGE_PAGE (Generic Desktop)
    0x09, 0x30,                    //     USAGE (X)
    0x09, 0x31,                    //     USAGE (Y)
@@ -112,6 +113,7 @@ static uint8_t keyboard_mouse_report_map_data[] = {
    0x75, 0x08,                    //     REPORT_SIZE (8)
    0x95, 0x03,                    //     REPORT_COUNT (3)
    0x81, 0x06,                    //     INPUT (Data,Var,Rel)
+   
    0xC0,                          //   END_COLLECTION
    0xC0,                     	  // END COLLECTION   
 };
@@ -163,6 +165,39 @@ static uint8_t report_map_data[] =
     0xC0              // End Collection (Application)
 };
 */
+
+
+/**@brief Function for handling the HID Report Characteristic Write event.
+ *
+ * @param[in]   p_evt   HID service event.
+ */
+static void on_hid_rep_char_write(ble_hids_evt_t * p_evt){
+    if (p_evt->params.char_write.char_id.rep_type != BLE_HIDS_REP_TYPE_OUTPUT){
+		NRF_LOG_INFO("rep_type: %d", p_evt->params.char_write.char_id.rep_type);
+		return;
+	}
+    
+	ret_code_t err_code;
+    uint8_t  report_val;
+    uint8_t  report_index = p_evt->params.char_write.char_id.rep_index;
+
+    if (report_index != OUTPUT_REPORT_INDEX){
+		NRF_LOG_INFO("report_index: %d", report_index);
+		return;
+	}
+    // This code assumes that the output report is one byte long. Hence the following
+    // static assert is made.
+    STATIC_ASSERT(OUTPUT_REPORT_MAX_LEN == 1);
+	err_code = ble_hids_outp_rep_get(&m_hids, report_index, OUTPUT_REPORT_MAX_LEN, 0, m_conn_handle, &report_val);
+    APP_ERROR_CHECK(err_code);
+
+    if (((report_val & OUTPUT_REPORT_BIT_MASK_CAPS_LOCK) != 0)){
+        NRF_LOG_INFO("Caps Lock is turned On!");
+    }else{
+		NRF_LOG_INFO("Caps Lock is turned Off!");
+    }
+}
+
 /**@brief Function for handling HID events.
  *
  * @details This function will be called for all HID events which are passed to the application.
@@ -183,6 +218,8 @@ static void on_hids_evt(ble_hids_t * p_hids, ble_hids_evt_t * p_evt)
             break;
 
         case BLE_HIDS_EVT_REP_CHAR_WRITE:
+			NRF_LOG_INFO("BLE_HIDS_EVT_REP_CHAR_WRITE");
+			//on_hid_rep_char_write(p_evt);
             break;
 
         case BLE_HIDS_EVT_NOTIF_ENABLED:
@@ -215,16 +252,16 @@ void hid_init(void){
     ble_hids_outp_rep_init_t * p_output_report;
     uint8_t                    hid_info_flags;
 
-    static ble_hids_inp_rep_init_t  input_report_array[2];
-    static ble_hids_outp_rep_init_t output_report_array[1];
+    static ble_hids_inp_rep_init_t  input_report_array[INPUT_REPORT_COUNT];
+    static ble_hids_outp_rep_init_t output_report_array[OUTPUT_REPORT_COUNT];
 	
     memset((void *)input_report_array, 0, sizeof(input_report_array));
     memset((void *)output_report_array, 0, sizeof(output_report_array));
 
     // Initialize HID keyboard
-    p_input_report                      = &input_report_array[INPUT_REPORT_KEYS_INDEX];
+    p_input_report                      = &input_report_array[INPUT_REPORT_KBD_INDEX];
     p_input_report->max_len             = INPUT_REPORT_KEYS_MAX_LEN;
-    p_input_report->rep_ref.report_id   = REPORT_ID_KEYBOARD;
+    p_input_report->rep_ref.report_id   = INPUT_REPORT_KEYBOARD_ID;
     p_input_report->rep_ref.report_type = BLE_HIDS_REP_TYPE_INPUT;
 
     BLE_GAP_CONN_SEC_MODE_SET_ENC_NO_MITM(&p_input_report->security_mode.cccd_write_perm);
@@ -233,7 +270,7 @@ void hid_init(void){
 
     p_output_report                      = &output_report_array[OUTPUT_REPORT_INDEX];
     p_output_report->max_len             = OUTPUT_REPORT_MAX_LEN;
-    p_output_report->rep_ref.report_id   = OUTPUT_REP_REF_ID;
+    p_output_report->rep_ref.report_id   = OUTPUT_REPORT_ID;
     p_output_report->rep_ref.report_type = BLE_HIDS_REP_TYPE_OUTPUT;
 
     BLE_GAP_CONN_SEC_MODE_SET_ENC_NO_MITM(&p_output_report->security_mode.read_perm);
@@ -241,8 +278,8 @@ void hid_init(void){
 
 	// Initialize HID mouse
 	p_input_report                      = &input_report_array[INPUT_REPORT_MOUSE_INDEX];
-    p_input_report->max_len             = 4;//--todo
-    p_input_report->rep_ref.report_id   = REPORT_ID_MOUSE;
+    p_input_report->max_len             = INPUT_REPORT_MOUSE_MAX_LEN;
+    p_input_report->rep_ref.report_id   = INPUT_REPORT_MOUSE_ID;
     p_input_report->rep_ref.report_type = BLE_HIDS_REP_TYPE_INPUT;
 	
 	BLE_GAP_CONN_SEC_MODE_SET_ENC_NO_MITM(&p_input_report->security_mode.cccd_write_perm);
@@ -258,9 +295,9 @@ void hid_init(void){
     hids_init_obj.error_handler                  = service_error_handler;
     hids_init_obj.is_kb                          = true;
     hids_init_obj.is_mouse                       = true;
-    hids_init_obj.inp_rep_count                  = 2;
+    hids_init_obj.inp_rep_count                  = INPUT_REPORT_COUNT;
     hids_init_obj.p_inp_rep_array                = input_report_array;
-    hids_init_obj.outp_rep_count                 = 1;
+    hids_init_obj.outp_rep_count                 = OUTPUT_REPORT_COUNT;
     hids_init_obj.p_outp_rep_array               = output_report_array;
     hids_init_obj.feature_rep_count              = 0;
     hids_init_obj.p_feature_rep_array            = NULL;
@@ -318,9 +355,9 @@ void send_keys(uint8_t * p_keys, uint8_t size, uint8_t modifier){
 	ret_code_t err_code;
     uint8_t  data[INPUT_REPORT_KEYS_MAX_LEN];
 	
-	ASSERT(size <= INPUT_REPORT_KEYS_MAX_LEN - SCAN_CODE_POS_BOOT_MODE);
+	ASSERT(size <= INPUT_REPORT_KEYS_MAX_LEN - SCAN_CODE_POS);
 	memset(data, 0, sizeof(data));
-	memcpy(data + SCAN_CODE_POS_BOOT_MODE, p_keys, size);
+	memcpy(data + SCAN_CODE_POS, p_keys, size);
 	data[0] = modifier;
 	
 	if (m_in_boot_mode){
